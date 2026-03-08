98e406f4c3a6e8ce1249ccc3695d5e242bc4fedd
Breiscot <omegaxgaming09@gmail.com>
2026-03-08 21:49:11 +0100

    added modifieds to the player_interaction

--- scripts/player_interaction.gd
+++ scripts/player_interaction.gd
@@ -1,65 +1,41 @@
-extends Node
+extends Node3D
 
 # Impostazioni
-@export var interaction_range: float = 3.0
-
-@onready var ray_cast: RayCast3D = $"../CameraPivot/Camera3D/InteractionRay"
+@onready var ray: RayCast3D = $"../CameraPivot/Camera3D/InteractionRay"
 @onready var inventory: Node = $"../PlayerInventory"
 
-# UI
-@export var prompt_label: Label
+var current_target = null
 
-# Stato
-var current_interactable: Node = null
-
-func _ready():
-	if ray_cast:
-		ray_cast.target_position = Vector3(0, 0, -interaction_range)
-		ray_cast.enabled = true
-		
 func _process(_delta):
-	if GameManager.is_game_over or GameManager.is_paused:
+	if GameManager.is_game_over or GameManager.has_won:
 		return
 		
-	_check_interactables()
-	
-	if Input.is_action_just_pressed("interact") and current_interactable:
-		_do_interact()
+	if ray and ray.is_colliding():
+		var collider = ray.get_collider()
 		
-func _check_interactables():
-	if not ray_cast or not ray_cast.is_colliding():
-		_clear_prompt()
-		return
-		
-	var collider = ray_cast.get_collider()
-	
-	if collider and collider.has_method("get_interaction_text"):
-		current_interactable = collider
-		_show_prompt(collider.get_interaction_text())
-	else:
-		# Controlla il parent (per Area3D)
-		if collider and collider.get_parent().has_method("get_interaction_text"):
-			current_interactable = collider.get_parent()
-			_show_prompt(current_interactable.get_interaction_text())
-		else:
-			_clear_prompt()
+		var interactable = null
+		if collider and collider.has_method("interact"):
+			interactable = collider
+		elif collider and collider.get_parent().has_method("interact"):
+			interactable = collider.get_parent()
 			
-func _do_interact():
-	if current_interactable and current_interactable.has_method("interact"):
-		current_interactable.interact(inventory)
+		if interactable:
+			current_target = interactable
+			return
+			
+	current_target = null
 		
-func _show_prompt(text: String):
-	if prompt_label:
-		prompt_label.text = "[E] " + text
-		prompt_label.visible = true
+func _unhandled_input(event):
+	if GameManager.is_game_over or GameManager.has_won:
+		return
 		
-func _clear_prompt():
-	current_interactable = null
-	if prompt_label:
-		prompt_label.visible = false
+	if not current_target:
+		return
 		
-func get_inventory() -> Node:
-	return inventory
-	
-func get_seal_needs() -> Node:
-	return GameManager.seal_needs
+	# E | Sfama
+	if event.is_action_pressed("interact"):
+		current_target.interact(inventory)
+		
+	# F | Parla
+	if event.is_action_pressed("talk") and current_target.has_method("talk"):
+		current_target.talk()
