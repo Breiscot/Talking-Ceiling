006af1e3f0a60070203206c07458d9994a96cfcb
Breiscot <omegaxgaming09@gmail.com>
2026-04-10 23:31:44 +0200

    I added two scripts, and another one for VR controllers

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,52 @@
 extends Node3D
 
+@onready var xr_origin = $XROrigin3D
+@onready var desktop_player = $Player
+@onready var vr_camera = $XROrigin3D/XRCamera3D
+@onready var desktop_camera = $Player/CameraPivot/Camera3D
+
+
 func _ready():
 	await get_tree().process_frame
+	
+	if VRManager.is_vr_available:
+		_setup_vr()
+	else:
+		_setup_desktop()
+		
 	GameManager.satisfaction = 0.0
+	
+func _setup_vr():
+	if VRManager.enable_vr():
+		# Attiva VR
+		xr_origin.visible = true
+		desktop_player.visible = false
+		vr_camera.current = true
+		
+		# Disabilita controller desktop
+		desktop_player.set_process(false)
+		desktop_player.set_physics_process(false)
+		
+		print("VR mode is ON")
+	else:
+		_setup_desktop()
+		
+func _setup_desktop():
+	# Modalità desktop
+	xr_origin.visible = false
+	desktop_player.visible = true
+	desktop_camera.current = true
+	
+	# Disabilita VR
+	VRManager.disable_vr()
+	
+	print("Desktop mode is ON")
+	
+# Switch con F11 VR/Desktop
+func _input(event):
+	if event.is_action_pressed("ui_fullscreen"):	#F11
+		if VRManager.is_vr_available:
+			if VRManager.is_vr_mode:
+				_setup_desktop()
+			else:
+				_setup_vr()
---
+++ scripts/vr_manager.gd
@@ -0,0 +1,45 @@
+extends Node
+
+var is_vr_available: bool = false
+var is_vr_mode: bool = false
+var xr_interface: XRInterface = null
+
+func _ready():
+	_check_vr()
+	
+func _check_vr():
+	xr_interface = XRServer.find_interface("OpenXR")
+	
+	if xr_interface and xr_interface.is_initialized():
+		is_vr_available = true
+		print("VR finded")
+	else:
+		is_vr_available = false
+		print("VR not finded, mode desktop")
+		
+func enable_vr() -> bool:
+	if not is_vr_available:
+		print("VR is not available")
+		return false
+		
+	if xr_interface.initialize():
+		is_vr_mode = true
+		get_viewport().use_xr = true
+		print("VR is ON")
+		return true
+	else:
+		print("Impossible initializing VR")
+		return false
+		
+func disable_vr():
+	if xr_interface and is_vr_mode:
+		get_viewport().use_xr = false
+		xr_interface.uninitialize()
+		is_vr_mode = false
+		print("Mode desktop ON")
+		
+func toggle_vr():
+	if is_vr_mode:
+		disable_vr()
+	else:
+		enable_vr()
