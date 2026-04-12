ad083527d12e7713ede084f5f9f3aaad4a77069d
Breiscot <omegaxgaming09@gmail.com>
2026-04-12 14:28:31 +0200

    I fixed the main problem, and the problem wasn't the scripts, but the configuration of the OculusTool, maybe I'll write something on README for let you find the solution

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,55 +1,80 @@
 extends Node3D
 
-@onready var xr_origin = $XROrigin3D
 @onready var desktop_player = $Player
-@onready var vr_camera = $XROrigin3D/XRCamera3D
-@onready var desktop_camera = $Player/CameraPivot/Camera3D
-
+@onready var xr_origin = $XROrigin3D
 
 func _ready():
 	await get_tree().process_frame
 	
-	if VRManager.is_vr_available:
-		_setup_vr()
-	else:
-		_setup_desktop()
-		
 	GameManager.satisfaction = 0.0
 	
-func _setup_vr():
-	if VRManager.enable_vr():
-		# Disabilita Desktop
-		desktop_player = get_node_or_null("Player")
-		if desktop_player:
-			desktop_player.visible = false
-			desktop_player.set_process(false)
-			desktop_player.set_physics_process(false)
-			
-			var desktop_cam = desktop_player.get_node_or_null("CameraPivot/Camera3D")
-			if desktop_cam:
-				desktop_cam.current = false
+	var xr = XRServer.find_interface("OpenXR")
+	
+	if xr and (xr.is_initialized() or xr.initialize()):
+		_start_vr()
+	else:
+		_start_desktop()
+	
+func _start_vr():
+	print("Start VR...")
+	
+	get_viewport().use_xr = true
+	
+	if desktop_player:
+		var cam = desktop_player.get_node_or_null("CameraPivot/Camera3D")
+		if cam:
+			cam.current = false
+			cam.set_process(false)
+		
+		desktop_player.set_process(false)
+		desktop_player.set_physics_process(false)
+		desktop_player.set_process_input(false)
+		desktop_player.set_process_unhandled_input(false)
+		desktop_player.visible = false
+		
+		for child in desktop_player.get_children():
+			if child.has_method("set_process"):
+				child.set_process(false)
+			if child.has_method("set_physics_process"):
+				child.set_physics_process(false)
 				
-		# Attiva VR
-		var xr_cam = get_node_or_null("XROrigin3D/XRCamera3D")
+	if xr_origin:
+		xr_origin.visible = true
+		var xr_cam = xr_origin.get_node_or_null("XRCamera3D")
 		if xr_cam:
 			xr_cam.current = true
 			
-		xr_origin.visible = true
-		desktop_player.visible = false
-		vr_camera.current = true
+	print("VR mode is ON")
 		
-		print("VR mode is ON")
-	else:
-		_setup_desktop()
-		
-func _setup_desktop():
-	# Modalità desktop
-	xr_origin.visible = false
-	desktop_player.visible = true
-	desktop_camera.current = true
+func _start_desktop():
+	get_viewport().use_xr = false
 	
-	# Disabilita VR
-	VRManager.disable_vr()
+	if xr_origin:
+		xr_origin.visible = false
+		xr_origin.set_process(false)
+		xr_origin.set_physics_process(false)
+		
+		var xr_cam = xr_origin.get_node_or_null("XRCamera3D")
+		if xr_cam:
+			xr_cam.current = false
+			
+	# Attiva desktop
+	if desktop_player:
+		desktop_player.visible = true
+		desktop_player.set_process(true)
+		desktop_player.set_physics_process(true)
+		desktop_player.set_process_input(true)
+		desktop_player.set_process_unhandled_input(true)
+		
+		var cam = desktop_player.get_node_or_null("CameraPivot/Camera3D")
+		if cam:
+			cam.current = true
+			
+		for child in desktop_player.get_children():
+			if child.has_method("set_process"):
+				child.set_process(true)
+			if child.has_method("set_physics_process"):
+				child.set_physics_process(true)
 	
 	print("Desktop mode is ON")
 	
@@ -57,8 +82,9 @@ func _setup_desktop():
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		if event.keycode == KEY_F11:
-			if VRManager.is_vr_available:
-				if VRManager.is_vr_mode:
-					_setup_desktop()
-				else:
-					_setup_vr()
+			if get_viewport().use_xr:
+				_start_desktop()
+			else:
+				var xr = XRServer.find_interface("OpenXR")
+				if xr and (xr.is_initialized() or xr.initialize()):
+					_start_vr()
