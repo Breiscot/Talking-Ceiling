b32f606fe56df6eee1273db2750b27063d1b24eb
Breiscot <omegaxgaming09@gmail.com>
2026-04-13 21:14:33 +0200

    I finished the vr_grabbable_item.gd for the moment, and now i'm doing other things

--- scripts/vr_grabbable_item.gd
+++ scripts/vr_grabbable_item.gd
@@ -14,3 +14,21 @@ func grab(controller):
 	is_held = true
 	holding_controller = controller
 	
+	# Disabilita fisica mentre é in mano
+	freeze = true
+	set_collision_layer_value(1, false)
+	set_collision_mask_value(1, false)
+	
+func release():
+	is_held = false
+	holding_controller = null
+	
+	# Riattiva fisica
+	freeze = false
+	set_collision_layer_value(1, true)
+	set_collision_mask_value(1, true)
+	
+func get_item_type() -> String:
+	if item_type == ItemType.FISH:
+		return "fish"
+	return "water"
